d7105ea28960fdab19eeb4a8747b30a4e4fa0f23
Rudalph <gonsalvesrudalph@gmail.com>
2024-10-15 00:14:38 +0530

    Chapter 0 & Chapter 1 Completed

---
+++ Basics/modules.py
@@ -0,0 +1,16 @@
+import datetime
+print(datetime.date.today())
+
+# ____________________________________________________________________________
+
+import os
+print("Current working directory: {0}".format(os.getcwd()))
+print (os.listdir())
+
+# ____________________________________________________________________________
+
+import pyfiglet
+ascii_art = pyfiglet.figlet_format("Udya Movie Baghaila yeshil pillu?")
+print(ascii_art)
+
+
---
+++ Basics/print.py
@@ -0,0 +1,5 @@
+print("Hello World!")
+
+print("""Hello world.
+How are you.
+Hope you are doing good""")
