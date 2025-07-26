468ecdf8600700100bf4066b611355f429455d72
Rudalph <gonsalvesrudalph@gmail.com>
2025-07-26 17:39:43 +0530

    coin change problem - Mini number of ways

---
+++ 18_Dynamic_Programming/02_Unbounded_Knapsack/03_Coin_Change.py
@@ -0,0 +1,13 @@
+#Coin Change Problem : Minimum number of coins required
+
+#Problem Statement
+#You are given an array of coins and a target and your task is to find the number of coins
+#such that the coins can be added up to get the target. You can use the coins infinite times
+
+'''
+Watch the video again
+https://www.youtube.com/watch?v=I-l6PBeERuc&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=16
+
+This question I have solved on Leetcode
+https://leetcode.com/problems/coin-change/submissions/1710905572/
+'''
