f8133ed257fe3ef8feae735910e259f9e0097108
Rudalph <gonsalvesrudalph@gmail.com>
2026-03-18 01:26:27 +0000

    Binary Search on Answer

---
+++ 20_Patterns/04_Binary_Search/14_binary_search_on_answer.py
@@ -0,0 +1,7 @@
+# Whenever we see a sorted array —— We implement Binary Search
+# But Binary Search can be applied in Unsorted array also
+# This is called Binary Search on Answer
+# In binary search we initilize start, end and mid
+# We compare mid with key, start, end then take decision —— This is Condition
+# In case of Binary Search on Answer we need to derive this condition —— Not that easy
+# After deriving that condition we can further decide and apply Binary Search on Unsorted Array
